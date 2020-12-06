fb43d92f95fe2c2fa3efa11ac002462cfb58f0d8
Jeremy Dye <dyeje@mail.gvsu.edu>
2020-12-06 16:50:52 -0500

    Day 6

---
+++ day6/surveys.rb
@@ -0,0 +1,35 @@
+def surveys
+  sum = 0
+  current_group = {}
+  File.readlines('input.txt').each do |line|
+    line = line.strip
+    if line.empty?
+      sum += current_group.keys.length
+      current_group = {}
+    else
+      line.each_char { |char| current_group[char] = true }
+    end
+  end
+
+  sum
+end
+
+def surveys_2
+  sum = 0
+  current_group = []
+  File.readlines('input.txt').each do |line|
+    line = line.strip
+    if line.empty?
+      answers = current_group.flat_map { |person| g.keys }.uniq
+      puts answers
+      sum += answers.map { |a| current_group.all? { |person| person[a] } || nil }.compact.length
+      current_group = []
+    else
+      person = {}
+      line.each_char { |char| person[char] = true }
+      current_group << person
+    end
+  end
+
+  sum
+end
